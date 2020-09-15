b06f14343bc83c917a5e7417a91d460fcd8cd5e3
harvin99 <hungtx1001@gmail.com>
2020-09-15 15:35:37 +0700

    Fix when not assign query

--- api/middlewares/api.blogs.middleware.js
+++ api/middlewares/api.blogs.middleware.js
@@ -1,28 +1,40 @@
 module.exports =  function paginatedResult(model) {
     return async (req, res, next) => {
-      const page = parseInt(req.query.page);
-      const limit = parseInt(req.query.limit);
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
-      const results = {};
-      if (endIndex < await model.countDocuments().exec()) {
-        results.next = {
-          page: page + 1,
-          limit: limit,
-        };
+      if(req.query.page && req.query.limit){
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const results = {};
+        if (endIndex < await model.countDocuments().exec()) {
+          results.next = {
+            page: page + 1,
+            limit: limit,
+          };
+        }
+        if (startIndex > 0) {
+          results.previous = {
+            page: page - 1,
+            limit: limit,
+          };
+        }
+        try {
+          results.results = await model.find().limit(limit).skip(startIndex).exec()
+          res.paginatedResults = results
+          next();
+      } catch (error) {
+            res.status(500).json({message: error.message})
+        }
       }
-      if (startIndex > 0) {
-        results.previous = {
-          page: page - 1,
-          limit: limit,
-        };
-      }
-      try {
-        results.results = await model.find().limit(limit).skip(startIndex).exec()
-        res.paginatedResults = results
-        next();
-    } catch (error) {
+      else {
+        try {
+          results.results = await model.find()
+          res.paginatedResults = results
+          next();
+        } catch (error) {
           res.status(500).json({message: error.message})
+        }
       }
+      
     };
   }
